0f8c9594ebdc0d3d88a357800b418465ecca9d47
FuckingWeeb5 <55873911+FuckingWeeb5@users.noreply.github.com>
2019-09-27 10:31:26 +0100

    Add files via upload

---
+++ Poopiedie.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 4chua57
+ */
+public class Poopiedie {
+    String x;
+ 
+    public void hallo(){
+        System.out.println("Litterally Die");
+    }
+} 
+    
+public static void main(String[] args){
+    
+    x ob = new Poopiedie();
+    x.hallo();
+   
+}
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
---
+++ Rejects.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solent.week1;
+
+/**
+ *
+ * @author 4chua57
+ */
+public class Rejects {
+    
+}
+
